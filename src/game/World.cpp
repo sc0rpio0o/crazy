599bafce62cd7d1e875e389f8e5b959fb498116e
Salja <beleko2009@web.de>
2010-05-03 14:06:53 +0200

    [Patch 006] Implementation of Spell Disabling System.
    Signed-off-by: Salja <beleko2009@web.de>

--- src/game/World.cpp
+++ src/game/World.cpp
@@ -1106,6 +1106,9 @@ void World::SetInitialWorldSettings()
     sLog.outString( "Loading Player level dependent mail rewards..." );
     sObjectMgr.LoadMailLevelRewards();
 
+    sLog.outString( "Loading Spell disabled..." );
+    sObjectMgr.LoadSpellDisabledEntrys();
+
     sLog.outString( "Loading Loot Tables..." );
     sLog.outString();
     LoadLootTables();
