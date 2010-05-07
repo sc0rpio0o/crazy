690e0e6adb51ba89e8217ef73e8eaed07682f3c6
Salja <beleko2009@web.de>
2010-05-07 16:27:20 +0200

    [Patch 011] Fixed Spell 8024 and Ranks.
    Signed-off-by: Salja <beleko2009@web.de>

--- addition/full_addition/1_Full_ManGos.sql
+++ addition/full_addition/1_Full_ManGos.sql
@@ -1,5 +1,9 @@
+-- 5_Autobroadcast_ManGos
+
 DELETE FROM `mangos_string` WHERE `entry` = 1300;
 INSERT INTO `mangos_string` VALUES (1300, '|cffffcc00[Server]: |cff00ff00%s|r', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+-- 6_Spell_Disabled_ManGos
 
 CREATE TABLE spell_disabled (
   entry int(11) unsigned NOT NULL default '0' COMMENT 'spell entry',
@@ -8,16 +12,23 @@ CREATE TABLE spell_disabled (
   PRIMARY KEY  (entry)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Disabled Spell System';
 
-INSERT INTO `mangos_string` VALUES 
+-- 9_Name_Announce_ManGos
 
+INSERT INTO `command` VALUES ('nameannounce',1,'Syntax: .nameannounce $MessageToBroadcast\r\n\r\nSend a global message to all players online in chat log with colored sender\'s name.');
+
+INSERT INTO `mangos_string` VALUES 
 (63,'|cffffff00 <Mod>|cffff0000[%s]|cffffff00 Announce:|cffff0000 %s|r',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL), 
 (64,'|cffffff00 <GM>|cffff0000[%s]|cffffff00 Announce:|cffff0000 %s|r',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL), 
 (65,'|cffffff00 <Admin>|cffff0000[%s]|cffffff00  Announce:|cffff0000 %s|r',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
 
-INSERT INTO `command` VALUES ('nameannounce',1,'Syntax: .nameannounce $MessageToBroadcast\r\n\r\nSend a global message to all players online in chat log with colored sender\'s name.');
+-- 10_Spell_Proc_Event_ManGos
 
 DELETE FROM `spell_proc_event` WHERE `entry` IN (48492,48494,48495);
 INSERT INTO `spell_proc_event` VALUES 
 (48492,0,7,524288,0,2048,16384,0,0,0,0),
 (48494,0,7,524288,0,2048,16384,0,0,0,0),
 (48495,0,7,524288,0,2048,16384,0,0,0,0);
+
+-- 11_Spell_Proc_Event_ManGos
+
+DELETE FROM `spell_bonus_data` WHERE `entry` = 8026;
